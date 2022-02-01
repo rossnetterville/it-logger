fbcc74f1a22f8630e3f685cd5e1ea46204485a19
Ross Netterville <ross_netterville@hotmail.com>
2022-02-01 15:35:06 +0200

    added Search action, reducer and updated component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -5,6 +5,7 @@ import {
   ADD_LOG,
   UPDATE_LOG,
   DELETE_LOG,
+  SEARCH_LOGS,
   SET_CURRENT,
   CLEAR_CURRENT
 } from './types';
@@ -71,6 +72,28 @@ export const deleteLog = (id) => async dispatch =>{
     dispatch({
       type: DELETE_LOG,
       payload: id
+    })
+
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    })
+  }
+}
+
+// Search logs
+export const searchLogs = (text) => async dispatch =>{
+  try {
+
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`)
+    const data = await res.json()
+    
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
     })
 
   } catch (err) {
