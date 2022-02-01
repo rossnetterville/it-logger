09a17f23facde5866e6a3cf46c8df22d4550867a
Ross Netterville <ross_netterville@hotmail.com>
2022-02-01 16:17:26 +0200

    added Techs reducer, action and updated TechListModal component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -27,7 +27,7 @@ export const getLogs = () => async dispatch =>{
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -77,7 +77,7 @@ export const deleteLog = (id) => async dispatch =>{
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -99,7 +99,7 @@ export const searchLogs = (text) => async dispatch =>{
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -128,7 +128,7 @@ export const updateLog = (log) => async dispatch =>{
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,32 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import { getTechs } from '../../actions/techActions';
 import TechItem from './TechItem';
 
 
 
-const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
+const TechListModal = ({getTechs, tech: {techs, loading}}) => {
+
 
   useEffect(() => {
     getTechs();
     // eslint-diable-next-line
   }, [])
 
-  const getTechs = async () => {
-    setLoading(true);
-    const res = await fetch('/techs');
-    const data = await res.json();
-    
-    setTechs(data);
-    setLoading(false)
-  }
-
   return (
     <div className="modal" id="tech-list-modal">
       <div className="modal-content">
         <h4>Technician List</h4>
         <ul className="collection">
-          {!loading && techs.map(tech => {
+          {!loading && 
+          techs !== null && 
+          techs.map(tech => {
             return <TechItem tech={tech} key={tech.id}/>
           })}
         </ul>
@@ -35,4 +30,13 @@ const TechListModal = () => {
   )
 }
 
-export default TechListModal
+TechListModal.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  tech: state.tech
+})
+
+export default connect(mapStateToProps, {getTechs})(TechListModal)
