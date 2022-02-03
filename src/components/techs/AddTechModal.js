df81061c82582d11cce37a91269bf7b45625683a
Ross Netterville <ross_netterville@hotmail.com>
2022-02-03 12:57:26 +0200

    added addTech to the Reducer, Action and to the AddTechModal

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({addTech}) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('')
   
@@ -10,7 +13,13 @@ const AddTechModal = () => {
     if(firstName === '' || lastName === ''){
       M.toast({html: "Please enter the first and last name"})
     } else{
-    console.log(firstName, lastName)
+    addTech({
+      firstName,
+      lastName
+    })
+
+    M.toast({html: `${firstName} ${lastName} added`})
+
 
     // Clear Fields
     setFirstName('')
@@ -54,5 +63,8 @@ const AddTechModal = () => {
   )
 }
 
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired
+}
 
-export default AddTechModal
+export default connect(null, {addTech})(AddTechModal)
